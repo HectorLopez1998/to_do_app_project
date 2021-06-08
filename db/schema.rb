5fd091e3fc0c771cbf6499a717719a3b4b33d6e8
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-06-07 20:51:48 -0400

    username column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_004801) do
+ActiveRecord::Schema.define(version: 2021_06_08_005015) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_004801) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
   end
 
 end
