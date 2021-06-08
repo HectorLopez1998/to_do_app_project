dc99a32571ef441d1bdda120dc172ebb4fb15ced
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-06-07 20:49:09 -0400

    created user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_004554) do
+ActiveRecord::Schema.define(version: 2021_06_08_004801) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2021_06_08_004554) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "list_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
