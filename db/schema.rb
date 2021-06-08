9835c87693410bd90ef0153f8431e5a7500c1ca8
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-06-07 20:54:05 -0400

    user_id to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_005015) do
+ActiveRecord::Schema.define(version: 2021_06_08_005305) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_005015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "list_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
