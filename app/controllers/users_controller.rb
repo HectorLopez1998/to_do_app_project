9d6d4d58f838098080d0ecd1b19e1ae4c0b0f168
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-06-11 17:36:26 -0400

    get and post '/login'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,28 @@ post '/signup' do
         redirect '/tasks'
     end
 end
+
+get '/login' do 
+    if logged_in?
+        redirect '/tasks'
+    else 
+        erb :'users/login'
+    end
+end
+
+post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect '/tasks'
+    else
+        redirect '/login'
+    end
+end
+
+
+
+
 end
 
 
