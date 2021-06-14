ca5b0cf369c6ed75e03f7074f0b20e10c1a48082
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-06-14 19:54:24 -0400

    index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,65 @@
 class TasksController < ApplicationController
     
-    get '/tasks' do
-        @tasks = Task.all 
-        @user = current_user
-        erb :'tasks/index'
-    end
-
-    get '/tasks/new' do
-      redirect_if_not_logged_in
+  get '/tasks' do
+      @tasks = Task.all 
       @user = current_user
-        @task = Task.new
-        erb :'tasks/new'
+      erb :'tasks/index'
+  end
+
+  get '/tasks/new' do
+    @user = current_user
+      @task = Task.new
+      erb :'tasks/new'
+  end
+
+  get '/tasks/:id' do
+   
+    @task = Task.find_by(id:params[:id])
+      @tasks = Task.all
+      @user = current_user
+      erb :'tasks/show'
+  end
+  
+  post '/tasks' do 
+      @task = Task.new(params)
+      @task.user_id = session[:user_id]
+      @task.save
+      redirect "/tasks/#{@task.id}"
     end
 
-    get '/tasks/:id' do
-        find_task
-        @tasks = Task.all
-        @user = current_user
-        erb :'tasks/show'
+    get '/tasks/:id/edit' do 
+      @task = Task.find_by(id:params[:id])
+      redirect_if_not_authorized
+      # not_authorized 
+       erb :"/tasks/edit"
     end
-    
-    post '/tasks' do 
-        @task = Task.new(params)
-        @task.user_id = session[:user_id]
-        @task.save
-        redirect "/tasks/#{@task.id}"
-      end
 
-      get '/tasks/:id/edit' do 
-        @task = Task.find_by(id:params[:id])
-        # not_authorized 
-         erb :"/tasks/edit"
-      end
+    patch '/tasks/:id' do 
+      @task = Task.find_by(id:params[:id])
+      redirect_if_not_authorized
+      @task.update(title: params[:title], description: params[:description])
+      redirect "/tasks/#{@task.id}"
+    end
 
-      patch '/tasks/:id' do 
-        @task = Task.find_by(id:params[:id])
-        @task.update(title: params[:title], description: params[:description])
-        redirect "/tasks/#{@task.id}"
-      end
+    delete '/tasks/:id' do 
+      @task = Task.find_by(id:params[:id])
+      @task.destroy
+      redirect '/tasks'
+    end
 
-      delete '/tasks/:id' do 
-        @task = Task.find_by(id:params[:id])
-        @task.destroy
+  private
+  def find_task
+      @task = Task.find_by(id:params[:id])
+  end
+
+  def redirect_if_not_authorized
+    if @task.user != current_user
+        flash[:error] = "You cant make this edit, you don't own this"
         redirect '/tasks'
-      end
+    end 
 
-    private
-    def find_task
-        @task = Task.find_by(id:params[:id])
-    end
+end 
 
-    
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,42 @@
 class UsersController < ApplicationController
-get '/signup' do
-    if logged_in?
-        redirect '/tasks'
-    else 
-        erb :'users/signup'
-    end
-end
+    get '/signup' do 
+        erb :"users/signup"
+    end 
+    
+    post '/signup' do 
+        user = User.new(params)
+        if user.username.blank? || user.password.blank? || User.find_by_username(params[:username])
+            redirect '/signup'
+        else 
+            user.save
+            session[:user_id] = user.id 
+            redirect '/tasks'
+        end 
+    end 
+    
+    get '/login' do
+        erb :"users/login"
+    end 
+    
+    
 
-post '/signup' do 
-    if params[:username] == "" || params[:password] == ""
-        redirect '/signup'
-    else 
-        @user = User.create(:username => params[:username], :password => params[:password])
-        session[:user_id] = @user.id
-        redirect '/tasks'
-    end
-end
-
-get '/login' do 
-    if logged_in?
-        redirect '/tasks'
-    else 
-        erb :'users/login'
-    end
-end
-
-post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect '/tasks'
-    else
-        redirect '/login'
-    end
-end
-
-get '/logout' do 
-    if session[:user_id] != nil
+    post '/login' do
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/tasks'
+        else 
+            
+            flash[:error] = "Invalid login"
+            redirect '/login'
+        end 
+    end 
+    
+    
+    get '/logout' do
         session.clear
-        redirect to '/login'
-    else 
-        redirect to '/'
-    end
-end
+        redirect '/login'
+    end 
 
-
-
-end
-
-
+    
+end 
